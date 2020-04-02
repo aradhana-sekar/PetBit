649c6a1b6f8d6fe4e9f35bc50a395f73fee9b274
aradhana-sekar <62485467+aradhana-sekar@users.noreply.github.com>
2020-04-01 17:53:57 -0700

    Update code.py

--- code.py
+++ code.py
@@ -17,13 +17,61 @@ def play_sound():
     time.sleep(0.5)
     cp.stop_tone()
     
+def laskoFanInfrared():
+    pulseIn.pause()  # pauses IR detection
+    cp.red_led = True
+    pulseOut.send(pulseArrayFan)  # sends IR pulse
+    time.sleep(0.2)  # wait so pulses don't run together
+    pulseIn.clear()  # clear detected pulses to remove partial artifacts
+    cp.red_led = False
+    pulseIn.resume()  # resumes IR detection
+    
 steps = 0
 dailytarget = 10000
 dayssinceepoch = int(time.time()/86400)
 display = True
 light = True
+fanOn = False
+maxTemp = 70
+feature = True
 
 while True:
+    if cp.button_b:
+        print("Button B Pressed!")
+        if feature:
+            feature = False
+        else:
+            feature = True
+    while True and feature:
+        if cp.touch_A4:
+            maxTemp = 60
+        if cp.touch_A5:
+            maxTemp = 70
+        if cp.touch_A6:
+            maxTemp = 80
+        cp.pixels.brightness = 0.3
+        temp_f = int(cp.temperature * (9 / 5) + 32)
+        print(temp_f, maxTemp)
+        if temp_f >= maxTemp and not fanOn:
+            print("It's", temp_f, ". Too hot! It's", maxTemp, ". Turning fan on now.")
+            laskoFanInfrared()
+            fanOn = True
+            cp.pixels.fill((0, 30, 30))
+        if temp_f < maxTemp and fanOn:
+            print("Not too hot!")
+            cp.pixels.fill((0, 0, 0))
+            laskoFanInfrared()
+            fanOn = False
+        if cp.button_b:
+            print("Turning off.")
+            if fanOn:
+                cp.pixels.fill((0, 0, 0))
+                laskoFanInfrared()
+                fanOn = False
+            time.sleep(5)
+            feature = False
+            break
+        time.sleep(0.5)
     if cp.switch == True:
         display = False
     else:
@@ -95,54 +143,3 @@ pulseArrayFan = array.array('H', [1296, 377, 1289, 384, 464, 1219, 1296, 379,
     462, 1227, 460, 1230, 456, 1234, 463, 1227, 460, 1227, 1287, 6964, 1294, 379, 1287,
     384, 464, 1219, 1296, 378, 1288, 392, 456, 1234, 463, 1227, 460, 1230, 457, 1232, 464,
     1226, 461, 1227, 1288])
-
-def laskoFanInfrared():
-    pulseIn.pause()  # pauses IR detection
-    cp.red_led = True
-    pulseOut.send(pulseArrayFan)  # sends IR pulse
-    time.sleep(0.2)  # wait so pulses don't run together
-    pulseIn.clear()  # clear detected pulses to remove partial artifacts
-    cp.red_led = False
-    pulseIn.resume()  # resumes IR detection
-
-fanOn = False
-maxTemp = 70
-feature = True
-
-while True:
-    if cp.button_b:
-        print("Button B Pressed!")
-        if feature:
-            feature = False
-        else:
-            feature = True
-    while True and feature:
-        if cp.touch_A4:
-            maxTemp = 60
-        if cp.touch_A5:
-            maxTemp = 70
-        if cp.touch_A6:
-            maxTemp = 80
-        cp.pixels.brightness = 0.3
-        temp_f = int(cp.temperature * (9 / 5) + 32)
-        print(temp_f, maxTemp)
-        if temp_f >= maxTemp and not fanOn:
-            print("It's", temp_f, ". Too hot! It's", maxTemp, ". Turning fan on now.")
-            laskoFanInfrared()
-            fanOn = True
-            cp.pixels.fill((0, 30, 30))
-        if temp_f < maxTemp and fanOn:
-            print("Not too hot!")
-            cp.pixels.fill((0, 0, 0))
-            laskoFanInfrared()
-            fanOn = False
-        if cp.button_b:
-            print("Turning off.")
-            if fanOn:
-                cp.pixels.fill((0, 0, 0))
-                laskoFanInfrared()
-                fanOn = False
-            time.sleep(5)
-            feature = False
-            break
-        time.sleep(0.5)
